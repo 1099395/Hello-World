9b71c00c4cf71e56de18a07120b19ddf1954d414
1099395 <n.chaitra1@tcs.com>
2017-11-09 16:06:54 +0530

    Create docker_build.bat

---
+++ docker_build.bat
@@ -0,0 +1,9 @@
+cd C:\Users\1197316\Downloads\Sample\Sample
+SET DOCKER_TLS_VERIFY=1
+SET DOCKER_HOST=tcp://192.168.99.100:2376
+SET DOCKER_CERT_PATH=C:\Users\1197316\.docker\machine\machines\default
+SET DOCKER_MACHINE_NAME=default
+SET COMPOSE_CONVERT_WINDOWS_PATHS=true
+REM Run this command to configure your shell:
+REM     @FOR /f "tokens=*" %i IN ('docker-machine env default') DO @%i
+%1\bin\mvn docker:build
